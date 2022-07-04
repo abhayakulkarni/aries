85fbcfc5ef6beb30d596139dc0d16de0af137e49
abhayakulkarni <abhaykulkarni10@gmail.com>
2022-07-03 20:39:09 -0400

    This is the third update to the client code

--- communication/client.py
+++ communication/client.py
@@ -3,34 +3,40 @@ import threading
 import time
 
 
-HOST = '172.29.62.243'
-PORT = 65433
-
+HOST = '172.29.62.243'  # The server's hostname or IP address
+PORT = 65434      # The port used by the server
 
 
 def process_data_from_server(x):
-	x1,y1 = x.split(",")
-	return x1,y1
+    x1, y1 = x.split(",")
+    return x1,y1
+
 
 def my_client():
-	threading.Timer(11, my_client).start()
-	
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
-		
-		my = input("Enter Command")
-		#my = "data"
-		my_inp =  my.encode('utf-8')
-		s.sendall(my_inp)
-		data = s.recv(1024).decode('utf-8')
-		x_temparature, y_humidity = process_data_from_server(data)
+    threading.Timer(11, my_client).start()
 
-		print("Temparature ()", format(x_temparature))
-		print("Humidity ()", format(y_humidity))
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
 
-		s.close()
-		time.sleep(5)
+        my = input("Enter command ")
 
-if __name__ == "__main__": 
-	while 1:
-		my_client()
+        #my = "Data"
+
+        my_inp = my.encode('utf-8')
+
+        s.sendall(my_inp)
+
+        data = s.recv(1024).decode('utf-8')
+
+        x_temperature,y_humidity = process_data_from_server(data)
+
+        print("Temperature {}".format(x_temperature))
+        print("Humidity {}".format(y_humidity))
+
+        s.close()
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    while 1:
+        my_client()
