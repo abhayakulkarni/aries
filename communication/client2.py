c01bd56180e993260ae342b7d4ee7cca3302da46
abhayakulkarni <abhaykulkarni10@gmail.com>
2022-07-12 00:47:42 -0400

    This is a new version of the client code

---
+++ communication/client2.py
@@ -0,0 +1,15 @@
+import socket 
+
+
+#HOST = '172.29.55.198'  # The server's hostname or IP address
+#PORT = 65435      # The port used by the server
+
+
+c = socket.socket()
+
+c.connect(('localhost',9999))
+
+name = input("Enter your name")
+c.send(bytes(name,'utf-8'))
+
+print(c.recv(1024).decode())
